48b93946b2c45ac1e760618dd2a31fc525d96e1e
KangSungLee <rkdtjd1020@naver.com>
2024-04-05 17:35:15 +0900

    react

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './apps/App21_router';
 import reportWebVitals from './reportWebVitals';
+import Button from './mui/Button';
+import TextInputs from './mui/TextInputs';
+import Badges from './mui/Badges';
+import Lists from './mui/Lists';
+import NestedList from './mui/NestedList';
+import CustomizedList from './mui/CustomizedList';
+import BasicTable from './mui/BasicTable';
+import DataTable from './mui/DataTable';
+import SimpleAlert from './mui/SimpleAlert';
+import BasicCard from './mui/BasicCard';
+import BasicModal from './mui/BasicModal';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div style={{marginTop: 40, marginLeft: 20,}}>
+     
+    </div>
   </React.StrictMode>
 );
 
